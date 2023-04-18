6f311479982c6f830015ef9fed337c2dc2b45dae
julianohe <104738855+julianohe@users.noreply.github.com>
2023-04-18 14:26:57 -0300

    Ecapsular/ dentro do html-css-javascript

---
+++ Componet Web-Frontend-dio/src/Components/TituloDinamico.js
@@ -0,0 +1,28 @@
+class TituloDinamico extends HTMLElement{
+    constructor () {
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+
+
+        //base component
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("titulo"); 
+        //this.getAttribute("titulo"); -> E chamado de PROP
+
+
+
+        //estilizar o component
+        const style = document.createElement("style");
+        style.textContent = `
+        h1{
+            color: red;
+        }
+        `;
+
+        //enviar para shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+customElements.define("titulo-dinamico", TituloDinamico);
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 <h2> O Basíco de Html!! </h2>
 EStrutura 
+
+<h2> O Avançado de Html!! </h2>
+
+<h3> Onpage </h3> 
+
+<h3> Boas praticas de acessibilidade </h3> 
+<h3> Segurança </h3> 
+<h3> PWA </h3> 
+<h3> Web Componentes </h3> 
 <h2> O Basíco de CSS!! </h2>
 Aparencia
 <h2> O Basíco de TepyScript!! </h2>
